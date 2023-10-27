b109c3c1e4338f2dcec83794098d299d703b5350
iam-mr-jhari <hjeevahari951@gmail.com>
2023-10-27 19:25:24 +0530

    first-commit

---
+++ public/assets/index.js
@@ -0,0 +1,7 @@
+import java from "./java.png";
+import html from "./html.png";
+import css from "./css.png";
+import sql from "./sql.png";
+import cpp from "./cpp.png";
+
+export { java, html, css, sql, cpp };
